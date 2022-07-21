db4904d555b22277dec0d92cf2596c8268a22720
andmatcha <jj.mcr185@gmail.com>
2022-07-22 00:58:55 +0900

    reactらしい記述に変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+    const [incompleteTodos, setIncompleteTodos] = useState(["aaa", "bbb"]);
+
+    const [completeTodos, setCompleteTodos] = useState(["ccc"]);
+
     return (
         <>
-            <div>
+            <div className="input-area">
                 <input type="text" placeholder="TODO" />
                 <button>追加</button>
             </div>
-            <div>
-                <h3>未完了のTODO</h3>
+            <div className="incomplete-area">
+                <h3 className="title">未完了のTODO</h3>
                 <ul>
-                    <li>
-                        <p>あああ</p>
-                        <button>完了</button>
-                        <button>削除</button>
-                    </li>
+                    {incompleteTodos.map((todo) => (
+                        <li key={todo} className="list-row">
+                            <p>{todo}</p>
+                            <button>完了</button>
+                            <button>削除</button>
+                        </li>
+                    ))}
                 </ul>
             </div>
-            <div>
-                <h3>完了のTODO</h3>
+            <div className="complete-area">
+                <h3 className="title">完了のTODO</h3>
                 <ul>
-                    <li>
-                        <p>あああ</p>
-                        <button>戻す</button>
-                    </li>
+                    {completeTodos.map((todo) => (
+                        <li key={todo} className="list-row">
+                            <p>{todo}</p>
+                            <button>戻す</button>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </>
