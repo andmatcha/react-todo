33ee78712767c9863a908807fab2c3aff990bf7f
andmatcha <jj.mcr185@gmail.com>
2022-07-22 00:48:16 +0900

    基本的な構造とcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import "./styles.css";
 
 export const App = () => {
     return (
         <>
-            <h1>Hello, world!</h1>
+            <div>
+                <input type="text" placeholder="TODO" />
+                <button>追加</button>
+            </div>
+            <div>
+                <h3>未完了のTODO</h3>
+                <ul>
+                    <li>
+                        <p>あああ</p>
+                        <button>完了</button>
+                        <button>削除</button>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h3>完了のTODO</h3>
+                <ul>
+                    <li>
+                        <p>あああ</p>
+                        <button>戻す</button>
+                    </li>
+                </ul>
+            </div>
         </>
     );
 };
